79e36e5c574ec5c958c3f16b7f8f0884128f2ffa
iTobby <coketack@gmail.com>
2020-10-22 13:21:01 +0900

    To practice for the props as 'Food I Like'

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ function Food({key, name, picture, rating}) {
 
   return (
     <div>
+      {/* <h3>{key}</h3> */}
       <h3>I like {name}.</h3>
-      <h4>{rating}/5.0</h4>
+      <h4>{rating} / 5.0</h4>
       <img src={picture} alt={name} />
+      <hr></hr>
     </div>
   )
 }
@@ -25,8 +27,26 @@ const foodILike = [
   {
     id: 2,
     name: 'Samgyeopsal',
-    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
+    image: 'http://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
     rating: '3.5',
+  },
+  {
+    id: 3,
+    name: 'Bibimbap',
+    image: 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
+    rating: '5.5',
+  },
+  {
+    id: 4,
+    name: 'Doncasu',
+    image: 'https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg',
+    rating: '3.7',
+  },
+  {
+    id: 5,
+    name: 'Kimbap',
+    image: 'http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg',
+    rating: '2.8',
   },
 ];
 
@@ -38,11 +58,17 @@ function App() {
     return (
       <div>
         {foodILike.map(dish => (
-            <Food key={dish.key} name={dish.name} picture={dish.image} rating={dish.rating} />
+            <Food /*key={dish.id}*/ name={dish.name} picture={dish.image} rating={dish.rating} />
           )
         )}
       </div>
     );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 export default App;
